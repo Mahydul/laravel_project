40c5c043877f12f245bbb8cbe1c5d7092ffe233e
Mahydul <mahydulislamshajal@gmail.com>
2020-02-26 15:56:47 +0600

    setup the layout

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/about',function(){
-    return view('about',[
-        'channel'=>"This is about"
-    ]);
+    return view('about');
 });
 Route::get('/contact',function(){
     return view('about',[
